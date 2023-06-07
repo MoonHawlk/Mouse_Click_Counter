8bbbb4ca470b2ef2e3207ec87b84a3765a312dfe
Filipe Moreno <filipemorenoprogramador@gmail.com>
2023-06-07 16:02:48 -0300

    .csv coluns, utf-8 format, allow subroutines

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import csv
 import datetime
 from pynput.mouse import Listener
+import os
 
 def on_click(x, y, button, pressed):
     if pressed:
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        click_data = [current_time, x, y]
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.time().strftime("%H:%M:%S.%f")[:-3]
+        click_data = [current_time.date(), formatted_time, x, y]
         save_to_csv(click_data)
 
 def save_to_csv(click_data):
-    with open('click_data.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
+    file_path = 'click_data.csv'
+    is_file_exist = os.path.isfile(file_path)
+
+    with open(file_path, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+
+        if not is_file_exist:
+            writer.writerow(['Data', 'Hora', 'Posição X', 'Posição Y'])
+
         writer.writerow(click_data)
 
 with Listener(on_click=on_click) as listener:
